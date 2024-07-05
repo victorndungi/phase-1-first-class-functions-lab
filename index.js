2306f83a76c36733c21a5c895fdc0b7d1407c4b0
victorndungi <victor.ndungi@student.moringaschool.com>
2024-07-05 12:47:12 +0300

    Add new index,js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2)
+}
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(-2)
+}
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
+function createFareMultiplier(multiplier){
+    return function(fare){
+        return fare * multiplier
+    }
+}
+function fareDoubler(fare){
+    return fare*2
+ }
+   
+    
+
+function fareTripler(fare){
+    return fare*3
+}
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2)
+}
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(-2)
+}
+function selectDifferentDrivers(arrayOfDrivers, fn) {
+    return fn(arrayOfDrivers)
+}
